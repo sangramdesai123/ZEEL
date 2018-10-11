862b0029c686b04ff48da445966e8d661c14086a
jeet82755 <jeet.gujarathi@somaiya.edu>
2018-10-11 15:02:29 +0530

    add and remove from cart logic

--- controller/signup.php
+++ controller/signup.php
@@ -1,14 +1,15 @@
 <?php
-if(isset($_GET['submit']))
+if(isset($_GET['name']))
 {
 	include_once 'db.php';
-	$name = mysqli_real_escape_string($con,$_POST['name']);
-	$email = mysqli_real_escape_string($con,$_POST['email']);
-	$pwd = mysqli_real_escape_string($con,$_POST['pwd']);
-	$tel = mysqli_real_escape_string($con,$_POST['phone']);
-	$address = mysqli_real_escape_string($con,$_POST['address']);
+	$name = mysqli_real_escape_string($con,$_GET['name']);
+	$email = mysqli_real_escape_string($con,$_GET['email']);
+	$pwd = mysqli_real_escape_string($con,$_GET['pwd']);
+	$tel = mysqli_real_escape_string($con,$_GET['phone']);
+	$address = mysqli_real_escape_string($con,$_GET['address']);
 	//error handeral
 	//cheack empty input
+	echo $name;
 	if(empty($name) ||empty($email)||empty($tel)||empty($pwd)||empty($address))
 	{
 		header("Location:loginpage.php?signup=empty");
@@ -26,7 +27,7 @@ if(isset($_GET['submit']))
 				exit();
 			}
 			else{
-				$sql="SELECT * FROM users WHERE name='$';";
+				$sql="SELECT * FROM users WHERE name='$name';";
 				$result=mysqli_query($con,$sql);
 				$resultCheck=mysqli_num_rows($result);
 				if($resultCheck>0)
@@ -35,11 +36,11 @@ if(isset($_GET['submit']))
 					exit();
 				}else{
 					//hashing password
-					$hashedpwd=password_hash($pwd,PASSWORD_DEFAULT);
+					//$hashedpwd=password_hash($pwd,PASSWORD_DEFAULT);
 					//INSERT USER INTO DATABASE
-					$sql="INSERT INTO users (`name`, `email`, `pwd`, `tel`, `address`) VALUES('$name','$email','$hashedpwd','$tel','$address');";
+					$sql="INSERT INTO users (`name`, `email`, `pwd`, `tel`, `address`) VALUES('$name','$email','$pwd','$tel','$address');";
 					mysqli_query($con,$sql);
-					header("Location:home.php?signup=Success");
+					header("Location:../home.php?signup=Success");
 					exit();
 				}
 			}
